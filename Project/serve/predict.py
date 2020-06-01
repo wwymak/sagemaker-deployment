12fb8e2d0d9c9f6a31ad7ca42b25720b969aee72
wwymak <wwymak@gmail.com>
2020-06-01 22:15:03 +0100

    project

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -65,13 +65,10 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
-    #       You should produce two variables:
-    #         data_X   - A sequence of length 500 which represents the converted review
-    #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    converted_review = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, converted_review)
+    
+    
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +84,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = model(data).cpu().detach().numpy()
+    result = np.rint(result).astype(int)
 
     return result
